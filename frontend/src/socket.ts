cf112a1a382e2828b5bc24b32a682e3b4d75c982
mikedubcurry <michaelcurry95@gmail.com>
2023-05-27 19:26:47 -0400

    add auth check

--- frontend/src/socket.ts
+++ frontend/src/socket.ts
@@ -2,4 +2,8 @@ import { io } from 'socket.io-client';
 
 const URL = import.meta.env.VITE_SERVER_URL || '';
 
-export const socket = io(URL);
+export const socket = io(URL, {
+    auth: {
+        token: 'secret'
+    }
+});
